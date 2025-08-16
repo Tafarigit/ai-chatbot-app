95911eb4acdfc208ceff01499a3e8fac4e1ab471
Tafari Excell <excell.tafari@gmail.com>
2025-08-16 07:21:28 -0400

    syntax revisio n text edit and upkeep

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,3 +1,7 @@
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
+
+
 CREATE TABLE users (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
@@ -6,12 +10,17 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE messages (
+
+   CREATE TABLE messages (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-    message TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    sender_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    receiver_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
 -- CREATE TABLE channels (
 --     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
 --     name VARCHAR(100) UNIQUE NOT NULL,
